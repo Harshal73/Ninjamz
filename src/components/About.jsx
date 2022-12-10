154ce1b05c3f990d776972786c6d5a100485fb46
Harshal A <harshal73gh@gmail.com>
2022-12-10 15:24:48 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
        <h1 className='text-4xl font-bold' >Ninjatrader + Orderflowfootprints : 15000 Rs or 180 usd /-</h1>
        <h1 className='text-4xl font-bold pt-10 '>Mzpack : 7500 rs or 91 usd /-</h1>
        <h1 className='text-4xl font-bold pt-10'>Neurostreet: 7500 rs  or 91 usd /-</h1>
-       
+       <h1 className='text-4xl font-bold pt-10'>Bookmap + TTW: 25000 rs  or 300 usd /-</h1>
        <div className='flex flex-col pt-10' >
         <h1 className='text-2xl font-bold pb-10' >Explanation video Malayalam</h1>
        <iframe width="350" height="215"  src="https://www.youtube.com/embed/kXyktnrM-dI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
